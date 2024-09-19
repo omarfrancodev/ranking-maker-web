7ca09f6ff4030790c88f8ec7680a8aaa50e09ba5
Omar Franco <fofe2803@gmail.com>
2024-09-18 21:19:53 -0600

    feat: add and create the page files for the views and set the content for the application, and routes settings with tab link componente for the pages

---
+++ src/pages/ChartsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ChartsPage = () => {
+  return (
+    <div>
+      <h1>Comming soon (Charts)</h1>
+      {/* Aquí agregarás el contenido más tarde */}
+    </div>
+  );
+};
+
+export default ChartsPage;
