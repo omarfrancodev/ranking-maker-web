442bfe6f920f8185b8cb10dea53bfa95fe177db9
Omar Franco <fofe2803@gmail.com>
2024-09-18 21:17:06 -0600

    feat: add and create templates and molecules for main and page views, and tabs for content and nav links navigation

---
+++ src/components/molecules/TabsLink.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const TabsComponentWithRouting = ({ tabs }) => {
+  return (
+    <div className="flex justify-between space-x-1 bg-white p-2 rounded-lg mb-4">
+      {tabs.map((tab) => (
+        <NavLink
+          key={tab.path}
+          to={tab.path}
+          className={({ isActive }) =>
+            `flex w-full align-middle justify-center items-center space-x-2 py-2 px-4 rounded-lg text-md font-medium ${
+              isActive
+                ? "bg-indigo-500 text-white shadow-md"
+                : "text-gray-500 hover:bg-indigo-300 hover:text-white hover:shadow-sm"
+            }`
+          }
+        >
+          {tab.icon && <tab.icon className="w-5 h-5" />}
+          <span>{tab.label}</span>
+        </NavLink>
+      ))}
+    </div>
+  );
+};
+
+export default TabsComponentWithRouting;
