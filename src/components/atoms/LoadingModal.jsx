3cadea28d7a4497559c2bd265187eafc1be370d1
Omar Franco <fofe2803@gmail.com>
2024-09-19 21:58:37 -0600

    feat: changes on te behavior of the dialog and notificactions on mobile screens, add responsive design on some elements, modify the loading component behavior and props

--- src/components/atoms/LoadingModal.jsx
+++ src/components/atoms/LoadingModal.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { Loader } from "lucide-react";
 
-const LoadingModal = ({ isLoading }) => {
+const LoadingModal = ({
+  isLoading,
+  message = "Cargando...",
+  overlay = true,
+}) => {
   if (!isLoading) return null;
 
   return (
-    <div className="fixed z-50 inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white p-4 rounded shadow-lg flex items-center space-x-2">
+    <div
+      className={`${
+        overlay
+          ? "fixed inset-0 z-50 bg-gray-900 bg-opacity-50 "
+          : "absolute w-full h-full"
+      } flex justify-center items-center`}
+    >
+      <div className="bg-white p-4 rounded flex items-center space-x-2">
         <Loader className="animate-spin w-6 h-6 text-blue-500" />
-        <span>Cargando...</span>
+        <span>{message}</span>
       </div>
     </div>
   );
